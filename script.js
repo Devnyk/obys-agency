7601b4177b6f81c557d1eb3aa420b72ff2c4146b
dev nayak <dev842825@gmail.com>
2024-10-08 12:11:19 +0530

    completed

--- script.js
+++ script.js
@@ -31,70 +31,60 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
 
 }
-function loadingAnimation(){
-  
-var tl = gsap.timeline()
-
-tl.from(".line h1", {
-  y: 150,
-  stagger: 0.2,
-  duration:0.6,
-  delay: 0.5 
-})
-
-tl.from("#line1-part1",{
-  opacity:0,
-  onStart: function(){
-    var h5timer =  document.querySelector("#line1-part1 h5")
-
-    var grow = 0
-    setInterval(function(){
-      if(grow <100){
-        h5timer.innerHTML = grow++
-      }
-      else{
-        h5timer.innerHTML = grow
-        
-      }
-    },35)
-  }
-})
-
-tl.to(".line h2",{
-  animationName: "anime",
-  opacity:1
-})
-
-tl.to("#loader", {
-  opacity:0,
-  duraction:0.5,
-  delay:0
-})
-
-tl.from("#page1",{
-  delay: 0.2,
-  y:1200,
-  opacity:0,
-  duration: 0.5,
-  ease: Power4
-})
-
-tl.to("#loader",{
-  display: "none"
-})
-
-tl.from("#nav",{
-  opacity: 0.6
-})
-
-tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
-  y: 140,
-  stagger: 0.2
-})
-
-tl.from("#hero1, #page2",{
-  opacity:0
-},"-=1.2")
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 0;
+      setInterval(function () {
+        if (grow < 100) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 20);
+    },
+  });
+  tl.to(".line h2", {
+    animationName: "loaderanime",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 2,
+  });
+  tl.from("#page1", {
+    delay: 0.1,
+    y: 1600,
+    duration: 0.5,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+  tl.from("#nav", {
+    opacity: 0,
+  });
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+    y: 140,
+    stagger: 0.2,
+  });
+  tl.from(
+    "#hero1, #page2",
+    {
+      opacity: 0,
+    },
+    "-=1.2"
+  );
 }
 function cursorAnimation(){
   // document.addEventListener("mousemove", function(dets){
